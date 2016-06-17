afe7b5aacc1f2e8d4c2826d3e3b0666d310ef719
Samer Salib <Samer@samers-mac-mini.local>
2016-06-16 21:36:35 -0500

    Initial Commit

---
+++ His Kids/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  His Kids
+//
+//  Created by Samer Salib on 6/16/16.
+//  Copyright © 2016 Samer Salib. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
